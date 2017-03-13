fdeabaa93565dd7cba3650ddb2ed6fb5e5b4439e
GARBAGERABBIT <a99182003@gmail.com>
2017-03-13 20:46:11 +0800

    Add files via upload

---
+++ lab03-1.ino
@@ -0,0 +1,31 @@
+const byte LED = 13;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+Serial.print("Hello, World!");
+pinMode(LED,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val;
+  if(Serial.available() )
+{ 
+   val = Serial.read();
+   switch(val)
+   {
+     case '1':
+    {
+      digitalWrite(LED, HIGH);
+      Serial.print("LED ON");
+      break;
+    }
+     case '0':
+    {
+      digitalWrite(LED, LOW);
+      Serial.print("LED OFF");
+      break;
+    }
+    }
+  }  
+}
