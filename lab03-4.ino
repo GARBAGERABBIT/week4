7a5297d1bee2e65103b91dc80051dbf7a5d8c6d1
GARBAGERABBIT <a99182003@gmail.com>
2017-03-13 20:47:07 +0800

    Add files via upload

---
+++ lab03-4.ino
@@ -0,0 +1,20 @@
+float sensorVoltage;
+float sensorValue;
+float R1=0;
+float R2=1141000;
+int Vin=5;
+int i;
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+ for(i=0;i<=5;i++)
+   sensorValue = 0.7* sensorValue + 0.3* analogRead(A1);
+   sensorVoltage = sensorValue*5/1024;
+   R1=sensorVoltage* (R2) / Vin ;
+   delay(500);
+   
+   Serial.println(sensorVoltage);
+   
+}
